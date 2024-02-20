de2788323a56f85359e8f9eeea2ba03c7777c729
ibrahimdev23 <ibtheonemohamed13@gmail.com>
2024-02-20 09:13:05 -0600

    added api call

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const app = require("./src/app")
 const db = require("./db/connection");
 const port = 3000;
 
-//TODO: Create your GET Request Route Below: 
+const Restaurant = require('./models/Restaurant')
 
+//TODO: Create your GET Request Route Below: 
+app.get("/restaurants", async (req, res) => {
+    const restaurants = await Restaurant.findAll();
+    res.json(restaurants)
+})
 
 app.listen(port, () => {
     db.sync();
